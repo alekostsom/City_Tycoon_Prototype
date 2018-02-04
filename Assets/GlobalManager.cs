1d2dfcbd47b7b4d37007b3f161ae03c3afcad649
Alexandros Tsompolis <alekostsom@gmail.com>
2018-02-04 03:40:36 +0200

    Built Game Mechanics. (place a building, 1 minute construction time, income generator, calculation are made offline too, under construction and income UIs)

--- Assets/GlobalManager.cs
+++ Assets/GlobalManager.cs
@@ -10,14 +10,17 @@ public class GlobalManager : MonoBehaviour {
 	// Singleton object
 	public static GlobalManager gm_Instance;
 	
+	// Reference to the Canvas object
+	public Canvas canvasObj;
+	
 	// The exact reference date, to calculate timestamps
 	System.DateTime referenceDate;
 	public System.DateTime ReferenceDate
 	{
 		get {return referenceDate;}
-		set {referenceDate = value;}
 	}
 	
+	// An enum that consists of the building types
 	public enum BuildingType {None, Apartment, LuxApartment, Hotel};
 	// The selected building type
 	BuildingType selectedBType;
@@ -27,13 +30,38 @@ public class GlobalManager : MonoBehaviour {
 		set {selectedBType = value;}
 	}
 	
-	//
+	// Buildings prefabs
 	public GameObject apartmentBuildingObj;
 	public GameObject luxAptBuildingObj;
 	public GameObject hotelBuildingObj;
 	
+	// Under Construction UI prefab
+	public GameObject underConstructionUI;
+	// Generated Income UI prefab
+	public GameObject generatedIncomeUI;
+	
 	// Editor exposed variable to get reference of the status text object
 	public Text statusText;
+	
+	/**** 
+		Grid related variables
+	****/	
+	// Number of ground celss on each line
+	public int cellsPerLine = 4;
+	// Number of ground celss on each row
+	public int cellsPerRow = 4; 
+	// An array of the grid's ground celss to manage their functions
+	GroundCell[,] groundCells;
+	// The grid parent object in hierarchy
+	public GameObject gridParent;
+	//The base prefab of each grid cell
+	public GroundCell groundCellPrefab;
+	
+	// Player's balance
+	int balance;
+	int totalBalance;
+	
+	public Text balanceText;
 	
 	// Use this for initialization
 	void Awake () {
@@ -41,11 +69,32 @@ public class GlobalManager : MonoBehaviour {
 		gm_Instance = this;
 		
 		// Set the 2nd of Febryary 2018 as the exact ref date, to calculate timestamps. (Keeps the variables' values small)
-		referenceDate = new System.DateTime(2018, 2, 2, 0, 0, 0, System.DateTimeKind.Utc);
+		referenceDate = new System.DateTime(2018, 2, 1, 0, 0, 0, System.DateTimeKind.Utc);
 		
 		// A usage example of the reference date. Get current timestamps in seconds
-		int cur_time = (int)(System.DateTime.UtcNow - referenceDate).TotalSeconds;
-		Debug.Log(cur_time);
+		//int cur_time = (int)(System.DateTime.UtcNow - referenceDate).TotalSeconds;
+		
+		// Initialise the array of ground cells and create the grid
+		groundCells = new GroundCell[cellsPerRow, cellsPerLine];
+		for (int i=0; i<cellsPerRow; i++) 
+		{
+			for (int j=0; j<cellsPerLine; j++)
+			{
+				// Intstantiate the grid cells
+				groundCells[i,j] = Instantiate(groundCellPrefab,gridParent.transform, false) as GroundCell;
+				// Calculate the position in the grid like a visual representaion of a 2D array. (0,0,0) is the central point.
+				groundCells[i,j].transform.localPosition = new Vector3(-cellsPerLine/2 + j, 0, cellsPerRow/2 - i); 
+				
+				// Set the x,y coordinates
+				groundCells[i,j].X_coord = i;
+				groundCells[i,j].Y_coord = j;
+				
+				// Rename ground cell object, to indicate its coordinates in the editor (not really important)
+				groundCells[i,j].name += "_" + i.ToString() + "_" + j.ToString();			
+			}
+		}
+		
+		StartCoroutine(CalculateBalance());
 	}
 	
 	// Update is called once per frame
@@ -59,5 +108,21 @@ public class GlobalManager : MonoBehaviour {
 		string article = "a";
 		if (selectedBType == BuildingType.Apartment){ article = "an";}
 		statusText.text = "Click on a land area to build " + article + " " + selectedBType;
+	}
+	
+	public void NewGame()
+	{
+		PlayerPrefs.DeleteAll();
+	}
+	
+	// Calculate total balance, with income and expenses
+	IEnumerator CalculateBalance(){
+		totalBalance = balance;
+		foreach (GroundCell gc in groundCells){
+			totalBalance += PlayerPrefs.GetInt("Income_" + gc.X_coord.ToString() + "_" + gc.Y_coord.ToString());
+		}
+		balanceText.text = "Coins: " + totalBalance;
+		yield return new WaitForSecondsRealtime(1f);
+		StartCoroutine(CalculateBalance());
 	}
 }
--- Assets/GroundCell.cs
+++ Assets/GroundCell.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GroundCell : MonoBehaviour {
 	
@@ -10,18 +11,73 @@ public class GroundCell : MonoBehaviour {
 	// The original color of the ground cube's material
 	Color32 originalColor;
 	
+	// The x,y coordinates of the grid cell. They will get their value at the time of their instantiation
+	int x_coord,y_coord;
+	public int X_coord
+	{
+		get {return x_coord;}
+		set {x_coord = value;}
+	}
+	public int Y_coord
+	{
+		get {return y_coord;}
+		set {y_coord = value;}
+	}
+	
+	
+	// Temp object reference
+	GameObject objToBuild;
+	// The Building object that is built on this spesific grid cell
+	GameObject building;
+	// The built building's type name
+	string buildingTypeString;
+	// The income amount to be generated every time unit
+	int incomeAmountPerSec;
+	// The 'UnderConstruction' UI element
+	GameObject underConstruction;
+	// The 'Generated Income' UI element
+	GameObject incomeUI;
+	public GameObject IncomeUI
+	{
+		get {return incomeUI;}
+	}
+	
+	public bool built = false;
+	
 	void Start () {
 		// Get the reference to the GlobalManager singleton class
 		gm_Instance = GlobalManager.gm_Instance;
 		
 		//Get the color from the 'Top' child object
 		originalColor = GetComponentsInChildren<Renderer>()[1].material.color; 
+		
+		// 
+		if (PlayerPrefs.HasKey("CreationTimestamp_" + x_coord.ToString() + "_" + y_coord.ToString()) ){
+			if (PlayerPrefs.HasKey("CompletionTimestamp_" + x_coord.ToString() + "_" + y_coord.ToString())){	
+				built = true;
+				GlobalManager.BuildingType type = (GlobalManager.BuildingType) System.Enum.Parse( typeof( GlobalManager.BuildingType ), PlayerPrefs.GetString("BuildingType_" + x_coord.ToString() + "_" + y_coord.ToString()) );
+				InstantiateBuilding(type);
+				// Generate and Calculate building's income
+				// First calculate the already generated income
+				int completionTimestamp = PlayerPrefs.GetInt("CompletionTimestamp_" + x_coord.ToString() + "_" + y_coord.ToString());
+				int now = (int)(System.DateTime.UtcNow - gm_Instance.ReferenceDate).TotalSeconds;
+				PlayerPrefs.SetInt("Income_" + x_coord.ToString() + "_" + y_coord.ToString(), (now - completionTimestamp) * incomeAmountPerSec); 
+				// Generate and calculate the rest in real time
+				StartCoroutine(CalculateIncome());
+			}
+			else{
+				
+				GlobalManager.BuildingType type = (GlobalManager.BuildingType) System.Enum.Parse( typeof( GlobalManager.BuildingType ), PlayerPrefs.GetString("BuildingType_" + x_coord.ToString() + "_" + y_coord.ToString()) );
+				InstantiateBuilding(type);
+				InstantiateUI();
+				
+				// Start a coroutine to complete the construction
+				StartCoroutine(CompleteConstruction());
+				// Start a recursive coroutine to update the under construction ui element
+				StartCoroutine(UpdateConstructionUI());
+			}
+		}
 	}
-	
-	// Temp object reference
-	GameObject objToBuild;
-	// The Building object that is built on this spesific grid cell
-	GameObject building;
 	
 	// Update is called once per frame
 	void Update () {
@@ -34,41 +90,140 @@ public class GroundCell : MonoBehaviour {
 		Debug.Log("Ready to start building: " + gm_Instance.SelectedBType);
 		if (!building)
 			StartBuilding(gm_Instance.SelectedBType);
-		else
-			building.GetComponent<Renderer>().materials[1].color = Color.black;
+
 	}
 	
 	// Create a mouse hover effect when the mouse is over a buildable area
 	void OnMouseEnter() {
         // Enlight the surface of the area
-		GetComponentsInChildren<Renderer>()[1].material.color = Color.white;
+		if (!built){
+			GetComponentsInChildren<Renderer>()[1].material.color = Color.white;
+		}
     }	
     void OnMouseExit() {
 		// Set back the original color
-        GetComponentsInChildren<Renderer>()[1].material.color = originalColor;
+        if (!built){
+			GetComponentsInChildren<Renderer>()[1].material.color = originalColor;
+		}
     }
 	
-	// Function that handles a building construction
+	// Function that handles a building construction initialisation
 	void StartBuilding(GlobalManager.BuildingType bt){
+		InstantiateBuilding(bt);
+		InstantiateUI();
+		
+		// Store the construction starting timestamp in seconds
+		PlayerPrefs.SetInt("CreationTimestamp_" + x_coord.ToString() + "_" + y_coord.ToString(), (int)(System.DateTime.UtcNow - gm_Instance.ReferenceDate).TotalSeconds);
+		
+		// Store the building's type
+		PlayerPrefs.SetString("BuildingType_" + x_coord.ToString() + "_" + y_coord.ToString(), bt.ToString());
+
+		// Start constructing
+		StartCoroutine(CompleteConstruction());
+		StartCoroutine(UpdateConstructionUI());
+	}
+	
+	void InstantiateBuilding(GlobalManager.BuildingType bt){
+		// Set the selected 3D object to instantiate
 		switch (bt)
 		{
 			case GlobalManager.BuildingType.Apartment:
 				objToBuild = gm_Instance.apartmentBuildingObj;
+				incomeAmountPerSec = 1;
+				buildingTypeString = "Apartment";
 				break;
 			case GlobalManager.BuildingType.LuxApartment:
 				objToBuild = gm_Instance.luxAptBuildingObj;
+				incomeAmountPerSec = 4;
+				buildingTypeString = "Luxury Apartment";
 				break;
 			case GlobalManager.BuildingType.Hotel:
 				objToBuild = gm_Instance.hotelBuildingObj;
+				incomeAmountPerSec = 10;
+				buildingTypeString = "Hotel";
 				break;
 		}
 		
 		if (objToBuild != null){
-			building = GameObject.Instantiate(objToBuild) as GameObject;
-			building.transform.SetParent (transform);
+			// Instantiate the selected building object
+			building = GameObject.Instantiate(objToBuild, transform, false) as GameObject;
+			// Translate the 3d building 0.5 meters up.
 			building.transform.localPosition = Vector3.up * 0.5f;
-			
-			//building.GetComponent<Renderer>().materials[0].color = Color.black;
 		}
+	}
+	
+	void InstantiateUI(){
+		//Instantiate the 'Under Construction' UI element and place it according to the current grid cell positin
+		underConstruction = GameObject.Instantiate(gm_Instance.underConstructionUI, gm_Instance.canvasObj.transform, false) as GameObject;
+		//convert grid cell object position in 3D Space to its correspondand sceen position. 
+		Vector2 screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		// Place the UI element at the exact screen position of the 3D object. In order to work, the UI element's anchor must be set at the bottom left corner of the screen. 
+		// Translate the UI element 100 pixels up.
+		underConstruction.GetComponent<RectTransform>().anchoredPosition = screenPoint + Vector2.up * 100;		
+	}
+	
+	public void InstantiateIncomeUI(){
+		//Instantiate the 'Generated Income' UI element and place it according to the current grid cell positin
+		incomeUI = GameObject.Instantiate(gm_Instance.generatedIncomeUI, gm_Instance.canvasObj.transform, false) as GameObject;
+		//convert grid cell object position in 3D Space to its correspondand sceen position. 
+		Vector2 screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		// Place the UI element at the exact screen position of the 3D object. In order to work, the UI element's anchor must be set at the bottom left corner of the screen. 
+		// Translate the UI element 100 pixels up.
+		incomeUI.GetComponent<RectTransform>().anchoredPosition = screenPoint + Vector2.up * 100;	
+		
+		// Update the text values;
+		incomeUI.GetComponentsInChildren<Text>()[0].text = buildingTypeString;
+		incomeUI.GetComponentsInChildren<Text>()[1].text = "Income: " + PlayerPrefs.GetInt("Income_" + x_coord.ToString() + "_" + y_coord.ToString());
+		StartCoroutine(UpdateUIIncome());
+	}
+	
+	// A coroutine that will update the UI in the construction phase
+	IEnumerator UpdateConstructionUI(){
+		int creationTimestamp = PlayerPrefs.GetInt("CreationTimestamp_" + x_coord.ToString() + "_" + y_coord.ToString());
+		int now = (int)(System.DateTime.UtcNow - gm_Instance.ReferenceDate).TotalSeconds;
+		if (underConstruction)
+		{
+			underConstruction.GetComponent<UnderConstruction>().percentText.text = ((now - creationTimestamp)*100/60).ToString() + "%";
+			underConstruction.GetComponent<UnderConstruction>().percentImage.sizeDelta = new Vector2(((now - creationTimestamp)*150/60), 36);// 150 is the maximum size of the foreground slider
+			yield return new WaitForSecondsRealtime(1f);
+			StartCoroutine(UpdateConstructionUI());
+		}
+	}
+	
+	// A coroutine that will complete the construction of a building
+	IEnumerator CompleteConstruction(){
+		int creationTimestamp = PlayerPrefs.GetInt("CreationTimestamp_" + x_coord.ToString() + "_" + y_coord.ToString());
+		int now = (int)(System.DateTime.UtcNow - gm_Instance.ReferenceDate).TotalSeconds;
+		
+		yield return new WaitForSecondsRealtime(/*timeToConstruct*/ 60 - (now - creationTimestamp));
+		
+		built = true;
+		
+		// Store the construction completion timestamp in seconds
+		PlayerPrefs.SetInt("CompletionTimestamp_" + x_coord.ToString() + "_" + y_coord.ToString(), creationTimestamp + 60);
+		
+		// Start generating income
+		PlayerPrefs.SetInt("Income_" + x_coord.ToString() + "_" + y_coord.ToString(), 0);
+		StartCoroutine(CalculateIncome());
+		
+		StopCoroutine(UpdateConstructionUI());
+		Destroy (underConstruction);		
+	}
+	
+	// A coroutine that calculates and updates income
+	IEnumerator CalculateIncome(){	
+		yield return new WaitForSecondsRealtime(1f);
+		PlayerPrefs.SetInt("Income_" + x_coord.ToString() + "_" + y_coord.ToString(), PlayerPrefs.GetInt("Income_" + x_coord.ToString() + "_" + y_coord.ToString()) + incomeAmountPerSec);
+		Debug.Log(x_coord.ToString() + "_" + y_coord.ToString() + " generated income: " + PlayerPrefs.GetInt("Income_" + x_coord.ToString() + "_" + y_coord.ToString()));
+		StartCoroutine(CalculateIncome());
+	}
+	
+	// A coroutine that updates the income text value of the UI object 
+	public IEnumerator UpdateUIIncome(){	
+		yield return new WaitForSecondsRealtime(1f);
+		if (incomeUI){
+			incomeUI.GetComponentsInChildren<Text>()[1].text = "Income: " + PlayerPrefs.GetInt("Income_" + x_coord.ToString() + "_" + y_coord.ToString());
+		}
+		StartCoroutine(UpdateUIIncome());
 	}
 }
